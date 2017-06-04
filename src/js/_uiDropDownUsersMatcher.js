2762e5545a140b0ea402563bf3c95f7aed3c5adc
Unknown <andrievskiy@i-dgtl.ru>
2017-06-04 17:53:30 +0300

    Добавлен поиск вариантов при трнслитерации

--- src/js/_uiDropDownUsersMatcher.js
+++ src/js/_uiDropDownUsersMatcher.js
@@ -37,6 +37,26 @@
     };
     var cyrillicToLatinKeyBoard = {};
 
+    var latinToCyrillicFirstMap = {
+        'yo': 'ё',
+        'zh': 'ж',
+        'kh': 'х',
+        'ts': 'ц',
+        'ch': 'ч',
+        'sch': 'щ',
+        'shch': 'щ',
+        'sh': 'ш',
+        'eh': 'э',
+        'yu': 'ю',
+        'ya': 'я',
+        "'": 'ь'
+    };
+
+    var multipleLatinChars = {
+        'z': ['ж', 'з'],
+        'c': ['ц', 'ч']
+    };
+
 
     Object.keys(latinToCyrillicKeyBoard).map(function (key) {
         var k = latinToCyrillicKeyBoard[key];
@@ -81,6 +101,54 @@
 
         return variables;
     }
+
+    function _latinToCyrillicVariants(str) {
+        var variants;
+        // Replace first special "big" symbols
+        Object.keys(latinToCyrillicFirstMap).forEach(function (char) {
+           str = str.split(char).join(latinToCyrillicFirstMap[char]);
+        });
+        var charts = str.split('');
+
+        variants = _extendVariants([charts], 0, charts.length);
+        variants = variants.map(function (variant) {
+            return _replace(variant.join(''));
+        });
+
+        return variants;
+
+
+        function _extendVariants(variants, idx, len) {
+            if(idx == len){
+                return variants;
+            }
+            var extended = [];
+            variants.forEach(function (variant) {
+                var currentChart = variant[idx];
+                if(multipleLatinChars[currentChart]){
+                    multipleLatinChars[currentChart].forEach(function (chart) {
+                        var v =variant.slice();
+                        v[idx] = chart;
+                        extended.push(v);
+                    });
+                }
+            });
+            if(!extended.length){
+                extended = variants;
+            }
+            return _extendVariants(extended, idx+1, len);
+        }
+
+        function _replace(str) {
+            var latinAlphabet = 'abvgdezijklmnoprstufhcyABVGDEZIJKLMNOPRSTUFHCYёЁ';
+            var rusAlphabet = 'абвгдезийклмнопрстуфхцыАБВГДЕЗИЙКЛМНОПРСТУФХЦЫеЕ';
+            for (var i = 0; i < latinAlphabet.length; i++) {
+                str = str.split(latinAlphabet.charAt(i)).join(rusAlphabet.charAt(i));
+            }
+            return str;
+        }
+    }
+
 
     window.uiDropDownUsersMatcher = uiDropDownUsersMatcher;
 })(window);
