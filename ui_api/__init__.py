184b85fb595564f0502c5c30697ab0b1b269ab21
andrievskiy <andrievskiy@i-dgtl.ru>
2017-06-10 18:17:15 +0300

    Добавлена отдача стринцы и статики сервером

--- ui_api/__init__.py
+++ ui_api/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask.json import JSONEncoder
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../dist/',  static_url_path='')
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 db = SQLAlchemy(app)
 
