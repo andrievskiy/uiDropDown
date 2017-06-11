ad6b1822909889ec3eb8d3c65ebf82a30ebfc4c3
andrievskiy <andrievskiy@i-dgtl.ru>
2017-06-11 19:22:19 +0300

    [REFACTORING] Добавил доку к uiDropDownTemplate

--- src/js/util/template/_uiDropDownTemplate.js
+++ src/js/util/template/_uiDropDownTemplate.js
@@ -1,4 +1,16 @@
+/**
+ * Модуль для работы с шаблонами
+ */
 ;(function (window) {
+
+    /**
+     * Рендеринг шаблонов
+     * @param template {string} - Шаблон для рендеринга
+     *                            Подстановки производяться по швблону {name}
+     *                            При этом при указании {name::html} для данного значения не будет производиться экранирование
+     * @param data {object} - Даные для рендеринга. Поиск даныных для подставновок производится по ключам этого объекта
+     * @returns {string}
+     */
     function renderTemplate(template, data) {
         return template.replace(/{([\w|:]+)}/g, function (match, key) {
 
@@ -10,5 +22,6 @@
             return uiDropDownHtmlEscaping(data[key] || '');
         })
     }
+
     window.uiRenderTemplate = renderTemplate;
 })(window);
